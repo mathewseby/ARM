e6c193673bc2ae13c939a8bed91e3cd8673d1e95
bonikj <bonikj@gmail.com>
2020-12-14 08:36:43 +0900

    Update installserver.sh

--- installserver.sh
+++ installserver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-var1=$(az vmss nic list -g boney-test --vmss-name cassandra --query [].{ip:ipConfigurations[0].privateIpAddress} -o tsv | awk -vORS=, '{ print $1 }' | sed 's/,$/\n/')
+#var1=$(az vmss nic list -g boney-test --vmss-name cassandra --query [].{ip:ipConfigurations[0].privateIpAddress} -o tsv | awk -vORS=, '{ print $1 }' | sed 's/,$/\n/')
 
-echo "simply put, just so: $var1" > /opt/test.txt
+#echo "simply put, just so: $var1" > /opt/test.txt
 sed -i 's/seeds: .\+/seeds: '10.42.2.10'/g' /etc/cassandra/conf/cassandra.yaml
