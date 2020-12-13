33f716ff7b8a8fada14d7b740761a5c2da5cf389
bonikj <bonikj@gmail.com>
2020-12-13 11:47:28 +0900

    test

--- installserver.sh
+++ installserver.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-var1=$(az vmss nic list -g $1 --vmss-name $2 --query [].{ip:ipConfigurations[0].privateIpAddress} -o tsv | awk -vORS=, '{ print $1 }' | sed 's/,$/\n/')
+# var1=$(az vmss nic list -g $1 --vmss-name $2 --query [].{ip:ipConfigurations[0].privateIpAddress} -o tsv | awk -vORS=, '{ print $1 }' | sed 's/,$/\n/')
 
-sed 's/seeds: .\+/seeds: '$var1'/g' /etc/cassandra/conf/cassandra.yaml
+sed 's/seeds: .\+/seeds: '10.42.2.4'/g' /etc/cassandra/conf/cassandra.yaml
