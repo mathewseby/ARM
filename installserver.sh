13a1e04f6df9082d32093d1d723f1c1b7ff48562
Mathew Sebastian <70686868+mathewseby@users.noreply.github.com>
2020-12-16 16:40:33 +0530

    Update installserver.sh

--- installserver.sh
+++ installserver.sh
@@ -9,9 +9,3 @@ rm -rf /data/*
 #echo "simply put, just so: $var1" > /opt/test.txt
 sed -i 's/seeds: .\+/seeds: 'cassandra-0.internal.scalar-labs.com,cassandra-1.internal.scalar-labs.com,cassandra-2.internal.scalar-labs.com'/g' /etc/cassandra/conf/cassandra.yaml
 systemctl start cassandra
-
-for i in 0 1 2
-do
-ip=$(az vm show -g $1 -n cassandra-$i --query privateIps -d --out tsv)
-az network dns record-set a add-record -g $1 -z interal.scalar-labs.com -n cassandra-lb -a $ip
-done
