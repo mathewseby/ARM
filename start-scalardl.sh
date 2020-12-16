33268fc4b28c7172ffbad2463147d0156fbc1e4c
Mathew Sebastian <70686868+mathewseby@users.noreply.github.com>
2020-12-16 16:49:15 +0530

    Create start-scalardl.sh

---
+++ start-scalardl.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Add user to docker group
+sudo usermod -aG docker $USER
+
+#export values for docker-compose
+export SCALAR_IMAGE=scalarlabs/scalar-ledger:2.1.0
+export SCALAR_DB_STORAGE=cassandra
+export SCALAR_DB_CONTACT_POINTS=cassandra-000000.internal.scalar-labs.com
+export SCALAR_DB_CONTACT_PORT=9042
+export SCALAR_DB_USERNAME=cassandra
+export SCALAR_DB_PASSWORD=cassandra
