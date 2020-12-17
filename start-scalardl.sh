30b843c59dd0b61d03221954b589aa63c333fd20
Mathew Sebastian <70686868+mathewseby@users.noreply.github.com>
2020-12-17 16:14:45 +0530

    Update start-scalardl.sh

--- start-scalardl.sh
+++ start-scalardl.sh
@@ -2,7 +2,8 @@
 
 # Add user to docker group
 sudo usermod -aG docker centos
-sudo systemctl restart docker
+sudo pkill docker
+sudo systemctl start docker
 
 #export values for docker-compose
 export SCALAR_IMAGE=scalarlabs/scalar-ledger:2.1.0
@@ -11,3 +12,6 @@ export SCALAR_DB_CONTACT_POINTS=cassandra-000000.internal.scalar-labs.com
 export SCALAR_DB_CONTACT_PORT=9042
 export SCALAR_DB_USERNAME=cassandra
 export SCALAR_DB_PASSWORD=cassandra
+
+#run docker compose
+docker-compose up -d
