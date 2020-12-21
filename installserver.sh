72ef314cea87c326f41d3fda8d96d4b24c4263b4
Mathew Sebastian <70686868+mathewseby@users.noreply.github.com>
2020-12-21 07:41:05 +0530

    Update installserver.sh

--- installserver.sh
+++ installserver.sh
@@ -2,9 +2,9 @@
 
 #var1=$(az vmss nic list -g boney-test --vmss-name cassandra --query [].{ip:ipConfigurations[0].privateIpAddress} -o tsv | awk -vORS=, '{ print $1 }' | sed 's/,$/\n/')
 
-systemctl stop cassandra
-rm -rf /commitlog/*
-rm -rf /data/*
+#systemctl stop cassandra
+#rm -rf /commitlog/*
+#rm -rf /data/*
 
 #echo "simply put, just so: $var1" > /opt/test.txt
 sed -i 's/seeds: .\+/seeds: 'cassandra-0.internal.scalar-labs.com,cassandra-1.internal.scalar-labs.com,cassandra-2.internal.scalar-labs.com'/g' /etc/cassandra/conf/cassandra.yaml
