3aed4163caf8c92ce9bff3fab41271969cccccc4
Mathew Sebastian <70686868+mathewseby@users.noreply.github.com>
2020-12-21 07:43:03 +0530

    Create start-envoy.sh

---
+++ start-envoy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#export values
+export ENVOY_IMAGE=envoyproxy/envoy
+export ENVOY_TAG=v1.14.1
+export ENVOY_PORT=50051
+export ENVOY_PRIVILEGED_PORT=50052
+
+#run docker compose
+cd /opt/scalar-envoy ; docker-compose up -d
