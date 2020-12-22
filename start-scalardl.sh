97dc1e3ab7d116dff6ae8874e3170b7523f09cf1
Mathew Sebastian <70686868+mathewseby@users.noreply.github.com>
2020-12-22 21:38:17 +0530

    Update start-scalardl.sh

--- start-scalardl.sh
+++ start-scalardl.sh
@@ -10,3 +10,6 @@ export SCALAR_DB_PASSWORD=cassandra
 
 #run docker compose
 cd /opt ; docker-compose up -d
+
+#Load schema
+docker run -e CASSANDRA_HOST=cassandra-000000.internal.scalar-labs.com -e CASSANDRA_PORT=9042 -e CASSANDRA_USERNAME=cassandra -e CASSANDRA_PASSWORD=cassandra -e CASSANDRA_REPLICATION_FACTOR=3 scalarlabs/scalardl-schema-loader-cassandra:1.0.0
