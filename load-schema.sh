4abbafe613e7727ec59c2809afc09fc764ccd037
Mathew Sebastian <70686868+mathewseby@users.noreply.github.com>
2020-12-23 15:55:49 +0530

    Create load-schema.sh

---
+++ load-schema.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#load schema
+docker run -e CASSANDRA_HOST=cassandra-000000.internal.scalar-labs.com -e CASSANDRA_PORT=9042 -e CASSANDRA_USERNAME=cassandra -e CASSANDRA_PASSWORD=cassandra -e CASSANDRA_REPLICATION_FACTOR=3 scalarlabs/scalardl-schema-loader-cassandra:1.0.0
