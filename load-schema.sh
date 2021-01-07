ba05e9ad1e5f38bebee4e50478db45ae9c6a96ba
Mathew Sebastian <70686868+mathewseby@users.noreply.github.com>
2021-01-07 11:04:04 +0530

    Update load-schema.sh

--- load-schema.sh
+++ load-schema.sh
@@ -5,4 +5,6 @@
 #load schema
 #docker run -e CASSANDRA_HOST=cassandra-000000.internal.scalar-labs.com -e CASSANDRA_PORT=9042 -e CASSANDRA_USERNAME=cassandra -e CASSANDRA_PASSWORD=cassandra -e CASSANDRA_REPLICATION_FACTOR=3 scalarlabs/scalardl-schema-loader-cassandra:1.0.0
 
-docker run --rm scalar-labs/scalardl-schema-loader:1.3.0 --cosmos -h https://mathew-cosmos-test.documents.azure.com:443/ -p JG8WtuOW3mwzXEvPAL10K4ffa8bcxzk6uN0gSZZWX80goYENaBgbM1Q4CkYsvrGu903GPptTnhj8bsH2XPhnrg== 
+var1= $(az cosmosdb show --name mathew-cosmos-test --resource-group mathew-arm-test | jq '.documentEndpoint')
+var2= $(az cosmosdb keys list --name mathew-cosmos-test --resource-group mathew-arm-test --subscription b647bf0e-0566-4b28-ac8c-37b24f3b7061 --type keys | jq '.primaryMasterKey')
+docker run --rm scalar-labs/scalardl-schema-loader:1.3.0 --cosmos -h $var1 -p $var2  
